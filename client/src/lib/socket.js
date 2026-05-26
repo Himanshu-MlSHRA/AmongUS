f0a72b54db7619b98ba1c3d4b2da8226ae2780cb
Himanshu Mishra <himanshu.2010m@gmail.com>
2026-05-26 18:55:11 +0530

    final production setup

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.VITE_SERVER_URL || 'https://amongus-w0vw.onrender.com';
+const URL = import.meta.env.VITE_SERVER_URL || 'https://amongus-coders-api.onrender.com';
 
 export const socket = io(URL, {
   autoConnect: true,
