98f81a21744bcd0e4db2bdf542b03d097afdfcf8
Himanshu Mishra <himanshu.2010m@gmail.com>
2026-05-26 18:45:26 +0530

    production urls fixed

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:3001';
+const URL = import.meta.env.VITE_SERVER_URL || 'https://amongus-w0vw.onrender.com';
 
 export const socket = io(URL, {
   autoConnect: true,
